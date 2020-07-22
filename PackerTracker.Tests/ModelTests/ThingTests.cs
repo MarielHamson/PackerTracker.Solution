421f886596d5e09be3d4aed8208a06006ff5961b
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-22 14:06:12 -0700

    successful test - Id of Thing added to list
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>
    Co-authored-by: James Henager <jameschenager@gmail.com>

--- PackerTracker.Tests/ModelTests/ThingTests.cs
+++ PackerTracker.Tests/ModelTests/ThingTests.cs
@@ -7,21 +7,33 @@ namespace PackerTracker.Tests
 {
   [TestClass]
 
-  public class ThingTests
+  public class ThingTests : IDisposable
+
   {
+    public void Dispose()
+    {
+      Thing.ClearAll();
+    }
 
     [TestMethod]
-    public void ThingConstructor_CreatesInstanceOfItem_Item()
+    public void ThingConstructor_CreatesInstanceOfThing_Thing()
     {
       Thing newThing = new Thing("BackPack", 5, true, 10);
       Assert.AreEqual(typeof(Thing), newThing.GetType());
     }
 
     [TestMethod]
-    public void ThingConstructor_CreatesInstanceOfItemWithName_Item()
+    public void ThingConstructor_CreatesInstanceOfThingWithName_Thing()
     {
       Thing newThing = new Thing("Backpack", 5, true, 10);
       Assert.AreEqual(newThing.Name, "Backpack");
+    }
+
+    [TestMethod]
+    public void ThingList_IncreasesIdOfThingWhenAddedToList_Thing()
+    {
+      Thing newThing = new Thing("Backpack", 5, true, 10);
+      Assert.AreEqual(newThing.Id, 1);
     }
   }
 }
